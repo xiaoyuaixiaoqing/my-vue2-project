ac187cb82ec24b07aaff1d25711afaa34f19da32
sunyuxin <sunyuxin@LHW.com>
2025-01-14 17:24:27 +0800

    --新建学校信息

---
+++ src/components/SchoolInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{schoolName}}
+    {{schoolAddress}}
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SchoolInfo",
+
+  data() {
+    return {
+      schoolName:'商洛学院',
+      schoolAddress:'陕西省商洛市'
+
+    }
+  },
+
+  methods: {},
+
+  created: {},
+}
+</script>
+
+<style scoped>
+
+</style>
